758d3edb57e4d0246bf9ef5cb8c5639f2585b524
Anders Norrback Bornholm <anders@bornholm.se>
2025-02-23 14:48:23 +0100

    Replaced favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#fff',
     icons: [
       {
-        src: '/favicon.ico',
+        src: '/traffic-light.png',
         sizes: 'any',
         type: 'image/x-icon',
       },
